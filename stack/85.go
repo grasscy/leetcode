edfbcc9cc7561b175dbabae756a579e22076fd62
grasscy <grassc@qq.com>
2020-12-02 22:56:35 +0800

    85

--- stack/85.go
+++ stack/85.go
@@ -1,5 +1,27 @@
 package stack
 
 func maximalRectangle(matrix [][]byte) int {
-	return 0
+	if len(matrix) == 0 {
+		return 0
+	}
+	max := 0
+	rowN := len(matrix)
+	colN := len(matrix[0])
+
+	hs := make([]int, colN)
+	for i := 0; i < rowN; i++ {
+		for j := 0; j < colN; j++ {
+			if matrix[i][j] == 1 {
+				hs[j]++
+			} else {
+				hs[j] = 0
+			}
+		}
+		r := largestRectangleArea1(hs)
+		if r > max {
+			max = r
+		}
+	}
+
+	return max
 }
