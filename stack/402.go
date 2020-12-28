d7f472c9812a3506e2b84e40d6c65f8499a07974
grasscy <grassc@qq.com>
2020-12-28 10:39:36 +0800

    402

--- stack/402.go
+++ stack/402.go
@@ -1,15 +1,66 @@
 package stack
 
+import "strings"
+
 func removeKdigits(num string, k int) string {
+	if len(num) == k {
+		return "0"
+	}
+
 	res := make([]uint8, 0, len(num))
 	for i := 0; i < len(num); i++ {
-		if len(res) > 0 && res[len(res)-1] > num[i] {
-			if k > 0 {
-				res = res[:len(res)-1]
-				k--
-			}
+		for len(res) > 0 && res[len(res)-1] > num[i] && k > 0 {
+			res = res[:len(res)-1]
+			k--
 		}
 		res = append(res, num[i])
 	}
-	return res
+
+	if k > 0 {
+		ii := 0
+		for i := len(res) - 1; i > 0; i-- {
+			if res[i] == '0' {
+				ii++
+			} else {
+				break
+			}
+		}
+
+		res = res[:len(res)-k-ii]
+	}
+
+	index := -1
+	for i, v := range res {
+		if v != '0' {
+			index = i
+			break
+		}
+	}
+	if index < 0 {
+		return "0"
+	}
+	if index > 0 {
+		res = res[index:]
+	}
+	return string(res)
+}
+
+func removeKdigits1(num string, k int) string {
+
+	remain := len(num) - k
+
+	res := make([]uint8, 0, len(num))
+	for i := 0; i < len(num); i++ {
+		for len(res) > 0 && res[len(res)-1] > num[i] && k > 0 {
+			res = res[:len(res)-1]
+			k--
+		}
+		res = append(res, num[i])
+	}
+	s := string(res[:remain])
+	s = strings.TrimLeft(s, "0")
+	if len(s) == 0 {
+		return "0"
+	}
+	return s
 }
