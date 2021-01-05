5647621d8ed4dc2d98591ec482d8da50336babe4
grasscy <grassc@qq.com>
2021-01-05 23:33:26 +0800

    503

--- stack/503.go
+++ stack/503.go
@@ -1,16 +1,52 @@
 package stack
 
-import "container/list"
-
 func nextGreaterElements(nums []int) []int {
-	stk := list.New()
-	cd := 0
-	res := make([]int, 0, len(nums))
-	for _, v := range nums {
-		if stk.Len() == 0 {
-			stk.PushBack(v)
-			cd = v
+	INT_MAX := int(^uint(0) >> 1)
+
+	stk := make([]int, 0, len(nums))
+	//stk = append(stk, INT_MAX)
+
+	res := make([]int, len(nums)+1)
+
+	nums = append([]int{INT_MAX}, nums...)
+
+	l := len(stk)
+	for i, v := range nums {
+		if l == 0 {
+			stk = append(stk, i)
+			l++
 			continue
 		}
+
+		if nums[stk[l-1]] < v {
+			for nums[stk[l-1]] < v {
+				res[stk[l-1]] = v
+				stk = stk[:l-1]
+				l--
+			}
+		}
+		stk = append(stk, i)
+		l++
 	}
+
+	for i := 1; i < len(nums)-1; i++ {
+		if l == 1 {
+			break
+		}
+
+		for nums[stk[l-1]] < nums[i] {
+			res[stk[l-1]] = nums[i]
+			stk = stk[:l-1]
+			l--
+		}
+	}
+
+	for l > 1 {
+		res[stk[l-1]] = -1
+		stk = stk[:l-1]
+		l--
+	}
+
+	return res[1:]
+
 }
