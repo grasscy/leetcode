f3a3650a7e09bfebc68b2b9e8b2c9d35afbc4830
grassc <grassc@qq.com>
2021-01-08 15:53:40 +0800

    636

--- stack/636.go
+++ stack/636.go
@@ -7,21 +7,50 @@ import (
 
 func exclusiveTime(n int, logs []string) []int {
 	res := make([]int, n)
-	stk := make([]int, 0, len(logs))
+	stk := make([]Info, 0, len(logs))
+	last := getInfo(logs[len(logs)-1])
 
-	for _, v := range logs {
-		split := strings.Split(v, ":")
-		id, _ := strconv.Atoi(split[0])
-		action := split[1]
-		t, _ := strconv.Atoi(split[2])
-		if action == "start" {
-			stk = append(stk, t)
-		} else {
-			ts := stk[len(stk)-1]
+	ts := make([]byte, last.T+1)
+	for i := range ts {
+		ts[i] = 255
+	}
+
+	for _, log := range logs {
+		info := getInfo(log)
+		if info.Type == "end" {
+			si := stk[len(stk)-1]
 			stk = stk[:len(stk)-1]
-			res[id] += ts
+
+			for i := si.T; i <= info.T; i++ {
+				if ts[i] > 111 {
+					ts[i] = info.Id
+				}
+			}
+		} else {
+			stk = append(stk, info)
 		}
+
+	}
+	for _, v := range ts {
+		res[v]++
 	}
 	return res
+}
 
+func getInfo(s string) Info {
+	split := strings.Split(s, ":")
+	id, _ := strconv.Atoi(split[0])
+	t, _ := strconv.Atoi(split[2])
+
+	return Info{
+		Id:   byte(id),
+		Type: split[1],
+		T:    t,
+	}
+}
+
+type Info struct {
+	Id   byte
+	Type string
+	T    int
 }
--- stack/636_test.go
+++ stack/636_test.go
@@ -15,7 +15,8 @@ func Test_exclusiveTime(t *testing.T) {
 		args args
 		want []int
 	}{
-		//2 ["0:start:0","1:start:2","1:end:5","0:end:6"] [3,4]
+		{"", args{1, []string{"0:start:0", "0:end:100000000"}}, []int{100000001}},
+		{"", args{2, []string{"0:start:0", "0:start:2", "0:end:5", "1:start:7", "1:end:7", "0:end:8"}}, []int{8, 1}},
 		{"", args{2, []string{"0:start:0", "1:start:2", "1:end:5", "0:end:6"}}, []int{3, 4}},
 	}
 	for _, tt := range tests {
