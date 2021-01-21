0d03af2ef4ef1754ffa114ec192997760bffabe0
grassc <grassc@qq.com>
2021-01-21 17:34:48 +0800

    57

--- sort/57.go
+++ sort/57.go
@@ -34,3 +34,38 @@ func insert(intervals [][]int, newInterval []int) [][]int {
 	}
 	return res
 }
+
+func insert2(intervals [][]int, newInterval []int) [][]int {
+	res := make([][]int, 0)
+	l := len(intervals)
+	i := 0
+	for i < l && intervals[i][1] < newInterval[0] {
+		res = append(res, intervals[i])
+		i++
+	}
+	for i < l && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(newInterval[0], intervals[i][0])
+		newInterval[1] = max(newInterval[1], intervals[i][1])
+		i++
+	}
+	res = append(res, newInterval)
+	for i < l {
+		res = append(res, intervals[i])
+		i++
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
