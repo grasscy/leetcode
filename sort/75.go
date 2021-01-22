acf4c069e1857e5b40c7aafa1414633f23e358bc
grasscy <grassc@qq.com>
2021-01-22 14:16:19 +0800

    75

--- sort/75.go
+++ sort/75.go
@@ -1,5 +1,23 @@
 package sort
 
-func sortColors(nums []int) {
+func sortColors(nums []int) []int {
+	i, j := 0, len(nums)-1
 
+	zero := 0
+
+	for i <= j {
+		if nums[i] == 0 {
+			nums[zero], nums[i] = nums[i], nums[zero]
+			zero++
+			i++
+		} else if nums[i] == 1 {
+			i++
+		} else {
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
+		}
+
+	}
+
+	return nums
 }
